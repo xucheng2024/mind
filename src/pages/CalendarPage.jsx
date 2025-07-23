44a110b03c3d8c56ddf506fe3058bd706c236ad9
xucheng2024 <eatfreshapple@gmail.com>
2025-07-23 14:57:25 +0800

    fix deploy bug

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
 import enUS from 'date-fns/locale/en-US';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { useSearchParams } from 'react-router-dom';
