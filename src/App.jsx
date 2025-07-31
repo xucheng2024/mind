ca1aaea14a3041d3cc5b5aab0b507b0c078e4d09
xucheng2024 <eatfreshapple@gmail.com>
2025-08-01 01:01:19 +0800

    Remove test PWA button and clean up debugging code

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import VersionUpdate from './components/VersionUpdate';
 import PWAUpdateNotification from './components/PWAUpdateNotification';
 import { OfflineIndicator } from './components';
 import SafariInstallGuide from './components/SafariInstallGuide';
-import PWATestButton from './components/PWATestButton';
 
 import { RegistrationProvider } from '../context/RegistrationContext';
 import { validateConfig } from './lib/config';
@@ -67,7 +66,6 @@ function App() {
       <VersionUpdate />
       <PWAUpdateNotification />
       <OfflineIndicator />
-      <PWATestButton />
     </>
   );
 }
